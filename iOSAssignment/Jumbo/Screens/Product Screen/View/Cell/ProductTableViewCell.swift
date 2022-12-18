566c332bb29da599201d84846a5ee94ec7383c54
Ramkrishna Baddi <RamkrishnaBaddi@gmail.com>
2022-12-18 14:54:17 +0530

    UI connected with mock data

--- iOSAssignment/Jumbo/Screens/Product Screen/View/Cell/ProductTableViewCell.swift
+++ iOSAssignment/Jumbo/Screens/Product Screen/View/Cell/ProductTableViewCell.swift
@@ -10,13 +10,32 @@ import UIKit
 
 final class ProductTableViewCell: BaseTableViewCell<ProductTableViewCellViewModel> {
     
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var quantity: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var unitPrice: UILabel!
+    @IBOutlet weak var stepper: StepperView!
+    
     // Use it instead of tableViewDelegate:didSelectRowAtIndex
     @IBAction func cellButtonDidTap(_ sender: AnyObject) {
         
     }
     
     override func bind() {
+        viewModel.productImage
+            .skip(1)
+            .subscribe(onNext: { [weak self] image  in
+                self?.productImage.image = image
+            }).disposed(by: disposeBag)
         
+        name.text = viewModel.name
+        quantity.text = viewModel.quantity
+        price.text = viewModel.price
+        unitPrice.text = viewModel.unitPrice
+        
+        stepper.setMaxCount(maxCount: viewModel.stepperMaxCount)
+        stepper.setMinCount(minCount: viewModel.stepperMinCount)
     }
     
 }
--- iOSAssignment/Jumbo/Screens/Product Screen/View/Cell/ProductTableViewCellViewModel.swift
+++ iOSAssignment/Jumbo/Screens/Product Screen/View/Cell/ProductTableViewCellViewModel.swift
@@ -13,15 +13,79 @@ class ProductTableViewCellViewModel: BaseTableViewCellViewModel {
     
     // MARK:- Dependency
     
+    private weak var imageManager: ImageManagerProtocol?
+    
     //MARK:- Input
     private let product: ProductRaw
-
-    // MARK- Output
     
+    // MARK- Output
+    var id: String { product.id }
+    var name: String { product.title }
+    var isQuantityHidden: Bool { product.quantity == nil }
+    var quantity: String { formatQuantity() }
+    var price: String { formatPrice() }
+    var unitPrice: String { formatUnitPrice() }
+    var productImage = BehaviorSubject<UIImage?>.init(value: nil)
+
     let cellButtonDidTap: PublishSubject<Void> = PublishSubject<()>()
     
-    init(product: ProductRaw){
+    let currencyFormatter: NumberFormatter = {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .currency
+
+        return numberFormatter
+    }()
+    
+    // Assuming values
+    let stepperMinCount = 0
+    let stepperMaxCount = 10
+    
+    init(product: ProductRaw
+        , imageManager: ImageManagerProtocol = ImageManager()){
         self.product = product
+        self.imageManager = imageManager
         super.init()
+        self.downloadProductImage()
+    }
+    
+    //
+    private func formatQuantity() -> String {
+        guard let quantity = product.quantity else { return "" }
+        return "Qty: \(quantity)"
+    }
+    
+    private func formatPrice() -> String {
+        guard let price = product.prices?.price?.amount,
+              let currency = product.prices?.price?.currency else {
+            return ""
+        }
+        
+        currencyFormatter.currencyCode = currency
+        return currencyFormatter.string(from: NSNumber(value: price)) ?? ""
+    }
+    
+    private func formatUnitPrice()  -> String {
+        guard let unitPrice = product.prices?.unitPrice,
+              let unit = unitPrice.unit,
+              let price = unitPrice.price?.amount else {
+            return ""
+        }
+        return "\(price)/ \(unit)"
+    }
+    
+    private func downloadProductImage() {
+        guard let primaryView = product.imageInfo?.primaryView,
+              let rawImage = primaryView.first,
+              let url = URL(string: rawImage.url),
+              let downloadImageManager = imageManager else { return  }
+        
+        downloadImageManager.loadImage(for: url, completion: { [weak self] response in
+            switch response {
+            case .success(let image):
+                self?.productImage.onNext(image)
+            case .failure(_): break
+                // Handle failure
+            }
+        })
     }
 }
