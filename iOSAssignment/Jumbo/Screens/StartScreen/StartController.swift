493d9d78e734d291596f5e6c61a9c4725a6946e2
EJ Cayabyab <elijahcayabyab@investagrams.com>
2023-01-19 13:59:37 +0800

    Implement start screen

---
+++ iOSAssignment/Jumbo/Screens/StartScreen/StartController.swift
@@ -0,0 +1,26 @@
+//
+//  StartController.swift
+//  iOSAssignment
+//
+//  Created by Spam C. on 1/19/23.
+//
+
+import UIKit
+import WebKit
+
+class StartScreen: UIViewController {
+    @IBOutlet weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         
+        guard let url = Bundle.main.url(forResource: "jumbo", withExtension: "gif"),
+           let data = try? Data(contentsOf: url),
+           let baseURL = Foundation.URL(string: "about:blank") else {
+          return
+        }
+        webView.load(data, mimeType: "image/gif", characterEncodingName: "UTF-8", baseURL: baseURL)
+//        webView.scalesPageToFit = true
+        webView.contentMode = UIView.ContentMode.scaleAspectFit
+      }
+}
