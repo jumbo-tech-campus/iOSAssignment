aaa269fc0657896fc3d7406c906f8b4b923c0559
EJ Cayabyab <elijahcayabyab@investagrams.com>
2023-01-20 15:37:42 +0800

    Use library to display gif

--- iOSAssignment/Jumbo/Screens/StartScreen/StartController.swift
+++ iOSAssignment/Jumbo/Screens/StartScreen/StartController.swift
@@ -6,19 +6,15 @@
 //
 
 import UIKit
-import WebKit
+import Kingfisher
 
 class StartScreen: UIViewController {
-    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var startImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
          
-        guard let url = Bundle.main.url(forResource: "jumbo", withExtension: "gif"),
-           let data = try? Data(contentsOf: url),
-           let baseURL = Foundation.URL(string: "about:blank") else {
-          return
-        }
-        webView.load(data, mimeType: "image/gif", characterEncodingName: "UTF-8", baseURL: baseURL)
+        guard let url = Bundle.main.url(forResource: "jumbo", withExtension: "gif") else { return }
+        startImageView.kf.setImage(with: url)
       }
 }
