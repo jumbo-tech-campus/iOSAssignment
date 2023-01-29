49d162aa2b42bcbe6fab8686700d23dcd7f2cecc
EJ Cayabyab <elijahcayabyab@investagrams.com>
2023-01-30 00:39:01 +0800

    Add unit testing with code coverage

---
+++ iOSAssignmentTests/ViewModels/ProductsControllerViewModelTests.swift
@@ -0,0 +1,37 @@
+//
+//  ProductsControllerViewModelTests.swift
+//  iOSAssignmentTests
+//
+//  Created by Spam C. on 1/30/23.
+//
+
+@testable import iOSAssignment
+import XCTest
+
+final class ProductsControllerViewModelTests: XCTestCase {
+    
+    var cartManager: CartManager!
+    var products: [ProductRaw]!
+    var viewModel: ProductsControllerViewModel!
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        cartManager = CartManager()
+        let productRepository = ProductsRepository()
+        products = productRepository.fetchRawProducts()?.products
+        viewModel = ProductsControllerViewModel()
+        viewModel.cartManager = cartManager
+    }
+    
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        cartManager = nil
+        products = nil
+    }
+    
+    func testLoadData() throws {
+        viewModel.loadData()
+        let expectedQuantity = products.count
+        XCTAssertEqual(expectedQuantity, viewModel.data.value.count)
+    }
+}
